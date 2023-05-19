f492e230c20b7bdbbc6a2be2b44982edd97b407e
Kwisoo <93458656+arvzy@users.noreply.github.com>
2023-05-19 21:16:26 +0800

    Add files via upload

---
+++ bstwbpg/scripts/script.js
@@ -0,0 +1,76 @@
+/* function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+  } */
+  
+function openTab(evt, tabName) {
+    var tab = document.getElementsByTagName("a")
+    for (i = 0; i < tab.length;  i++) {
+        tab[i].className = tab[i].className.replace(" active", "")
+    }
+    evt.currentTarget.className += " active";
+}
+//intro animation thing
+let intro = document.querySelector('.intro');
+let logo = document.querySelector('.logo-header');
+let logoSpan = document.querySelectorAll('.logo');
+
+window.addEventListener('DOMContentLoaded', ()=>{
+    
+    setTimeout(()=>{
+        logoSpan.forEach((span, idx)=>{
+            setTimeout(()=>{
+                span.classList.add('active');
+            }, (idx + 1) * 400)
+        });
+
+        setTimeout(()=>{
+            logoSpan.forEach((span, idx)=>{
+
+                setTimeout(()=>{
+                    span.classList.remove('active');
+                    span.classList.add('fade');
+                }, (idx + 1) * 50)
+            })
+        }, 2000);
+
+        setTimeout(()=>{
+            intro.style.top = '-100vh';
+        }, 2300);
+    })
+})
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+window.onscroll = function() {stickyHeader()};
+
+var header, sticky;
+
+header = document.getElementById("top-header");
+sticky = header.offsetTop;
+
+function stickyHeader() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+
+}

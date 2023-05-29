c67a98ead6dacd97c2ea0484288339399902209e
Arvin Gu <ArvinZero28@gmail.com>
2023-05-30 01:27:53 +0800

    asdajsldoihjaldisjfkhalksdjfhladsf

--- bstwbpg/scripts/script.js
+++ bstwbpg/scripts/script.js
@@ -78,8 +78,39 @@ function stickyHeader() {
 $(window).scroll(function(){
     if($(window).scrollTop()){
         $("nav").addClass("nav-bg");
+        $("nav").removeClass("nav-hide");
     }
     else{
         $("nav").removeClass("nav-bg");
+        $("nav").addClass("nav-hide");
     }
 })
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
